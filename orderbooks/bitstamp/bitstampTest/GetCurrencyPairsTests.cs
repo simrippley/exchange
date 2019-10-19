ddb79f2e9e079199fd842a85ef74f9740c6a8c8e
Andrew <andrewchess25@gmail.com>
2019-10-20 00:38:36 +0300

    Get currency pairs test method was written.

---
+++ orderbooks/bitstamp/bitstampTest/GetCurrencyPairsTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using UOB.Exchanges.Bitstamp;
+
+namespace bitstampTests
+{
+    /// <summary>
+    /// Class represents a list of tests
+    /// </summary>
+    public class GetCurrencyPairsTests
+    {
+        /// <summary>
+        /// Represents library class
+        /// </summary>
+        private Library _library;
+
+        /// <summary>
+        /// Represents basic setup
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            _library = new Library();
+        }
+
+        /// <summary>
+        /// Check getting a list of currencies
+        /// </summary>
+        [Test]
+        public void GetCurrencyPairs()
+        {
+            // Act
+            var _tradingPairs = _library.GetCurrencyPairs().Result;
+
+            // Assert
+            Assert.IsNotNull(_tradingPairs);
+            Assert.IsTrue(_tradingPairs.Count > 0);
+            foreach (var item in _tradingPairs)
+            {
+                Assert.IsNotEmpty(item.Name);
+            }
+        }
+    }
+}
